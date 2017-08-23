08ecbf5998d9e1f848da6da4a701ddc77a44f713
Zain ul abidin <zain@aligent.com.au>
2017-08-23 12:26:49 +0930

    builder: override php builder class

--- builder/docker_run.php
+++ builder/docker_run.php
@@ -1,5 +1,11 @@
 <?php
 require_once dirname(__FILE__) . '/DockerBuilder.php';
-require_once dirname(__FILE__) . '/DockerBuilderOverride.php';
+$vOverRide = dirname(dirname(dirname(__FILE__))) . '/DockerBuilderOverride.php';
+if (file_exists($vOverRide)){
+    require_once $vOverRide;
+}
+else{
+    require_once dirname(__FILE__) . '/DockerBuilderOverride.php';
+}
 $builder = new DockerBuilderOverride();
 echo $builder->executeAction();
